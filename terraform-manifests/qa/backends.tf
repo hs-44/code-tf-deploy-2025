03fb27687c77ff75ef18b313655d976f98bc5527
hs-44 <harshvardhansutar4444@gmail.com>
2025-05-25 18:20:47 +0530

    Update backends.tf

--- terraform-manifests/qa/backends.tf
+++ terraform-manifests/qa/backends.tf
@@ -10,7 +10,7 @@
 
 terraform {
   backend "s3" {
-    bucket = "aws-devops-testbucket-cb"
+    bucket = "aws-devops-testbucket-for-cb-25"
     key    = "terraformstates/qa.tfstate"
     region = "ap-south-1"
   }
